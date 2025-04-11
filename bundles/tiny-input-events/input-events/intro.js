5a70198a70755118f0fd5d5dca4ce1c414d1a4ac
slinkous <ZazLinkous@gmail.com>
2025-04-10 20:19:53 -0700

    Add websockets and Phobos logo

--- bundles/tiny-input-events/input-events/intro.js
+++ bundles/tiny-input-events/input-events/intro.js
@@ -1,8 +1,11 @@
 'use strict';
 
+const fs = require('fs')
+
 module.exports = {
   event: state => socket => {
-    socket.write("Welcome to the game!\r\n");
+    const motd  = fs.readFileSync(__dirname + '/../resources/motd').toString('utf8');
+    socket.write( motd + "\r\n");
     return socket.emit('login', socket);
   },
 };
